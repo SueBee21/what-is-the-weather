bd4dbaec255472e75489dfad7b2e445817eff7df
SusanBeno <beno.susan@gmail.com>
2020-09-12 21:29:11 -0400

    updating html and css

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+var myCity;
+
+var submitButton = getElementById("submit-button");
+submitButton.addEventListener("click", updateHistory);
+
+
+function updateHistory() {
+    item.textContent = myCity;
+    history.appendChild(item);
+};
+
+submitButton.addEventListener("click", displayCity);
+
+function displayCity() {
+    var myCity = document.getElementById("my-search").value;
+    console.log(myCity);
+    h5.appendChild(MyCity);
+};
+
+event.preventDefault();
+
+var apiKey = "1bf3c720e5b6cf2622ee8bd1f82b9ad5";
+$.ajax({
+    url:"https://api.openweathermap.org/data/2.5/weather?q"+myCity+"&units=imperial&appid=" + apiKey,
+    method:"GET"
+}).then(function(results){
+    console.log(results)
+    console.log(results.main.humidity)
+    console.log(results.name);
+    console.log(results.weather[0].icon);
+    var iconcode = results.weather[0].icon
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(results.wind.speed);
+    var lat = results.coord.lat;
+    var lon = results.coord.lon;
+    console.log(lat, lon);
+    $.ajax({
+        url:"https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon + "&appid=" + apiKey,
+        method:"GET"
+    }).then(function(uvResults){
+        console.log(uvResults);
+        console.log(uvResults.value);
+    })
+});
+
+
+$.ajax({
+    url:"https://api.openweathermap.org/data/2.5/forecast?q="+myCity+"&appid=" +apiKey,
+    method:"GET"
+}).then(function(fiveDayResult){
+    console.log(fiveDayResult);
+});
+
+
+
+
