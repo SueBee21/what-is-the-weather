4cb889c874ffa45bc662d7a051f39adc3ca79f89
SusanBeno <beno.susan@gmail.com>
2020-09-12 21:49:49 -0400

    updating js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,31 @@
-var myCity;
+var myCity = document.getElementById("my-search").value;
+
 
 var submitButton = getElementById("submit-button");
 submitButton.addEventListener("click", updateHistory);
 
 
 function updateHistory() {
-    item.textContent = myCity;
+    localStorage.setItem("mySavedCity", myCity)
+    item.textContent = mySavedCity;
     history.appendChild(item);
 };
 
 submitButton.addEventListener("click", displayCity);
 
 function displayCity() {
-    var myCity = document.getElementById("my-search").value;
+
     console.log(myCity);
     h5.appendChild(MyCity);
 };
 
 event.preventDefault();
 
+var myCity = "New Haven";
+
 var apiKey = "1bf3c720e5b6cf2622ee8bd1f82b9ad5";
 $.ajax({
-    url:"https://api.openweathermap.org/data/2.5/weather?q"+myCity+"&units=imperial&appid=" + apiKey,
+    url:"https://api.openweathermap.org/data/2.5/weather?q="+ myCity +"&units=imperial&appid=" + apiKey,
     method:"GET"
 }).then(function(results){
     console.log(results)
@@ -45,7 +49,7 @@ $.ajax({
 
 
 $.ajax({
-    url:"https://api.openweathermap.org/data/2.5/forecast?q="+myCity+"&appid=" +apiKey,
+    url:"https://api.openweathermap.org/data/2.5/forecast?q="+ myCity +"&appid=" +apiKey,
     method:"GET"
 }).then(function(fiveDayResult){
     console.log(fiveDayResult);
