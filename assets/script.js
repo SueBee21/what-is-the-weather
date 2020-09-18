e13b2aadbdc5552d2f4b843bfd6927ba1b907143
SusanBeno <beno.susan@gmail.com>
2020-09-17 20:18:40 -0400

    cleaning up js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 // define city
-
 var myCity;
 var cityName;
 var storedCities = [];
 var today = moment().format('MMMM Do YYYY');
 $("#current-day").text(today);
 
+//  local storage for search history
 function loadCities() {
     if (localStorage.getItem("storedCity")) {
         storedCities = JSON.parse(localStorage.getItem("storedCity"));
@@ -14,7 +14,7 @@ function loadCities() {
             var a = $("<a href='#' class='list-group-item list-group-item-action stored-city'>").text(storedCities[i]);
             $("#search-history").append(a);
         };
-        $(".stored-city").on("click", function(){
+        $(".stored-city").on("click", function () {
             var city = $(this).text();
             loadWeather(city);
         })
@@ -27,7 +27,6 @@ loadCities();
 var submitButton = $("#submit-button");
 
 
-// store and push city namae to history list and current city
 submitButton.on("click", function () {
     event.preventDefault();
     myCity = $("#my-search").val();
@@ -40,9 +39,7 @@ submitButton.on("click", function () {
     };
 
     loadWeather(myCity);
-
 });
-
 
 function loadWeather(myCity) {
     loadCities();
@@ -53,50 +50,40 @@ function loadWeather(myCity) {
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + myCity + "&units=imperial&appid=" + apiKey,
         method: "GET"
     }).then(function (results) {
-        console.log(results)
 
         // city name
-        console.log(results.name);
         cityName = results.name;
 
-        $("#history").text(cityName);
+        // populate result city name
         $("#my-city").text(cityName);
 
         // temp
         var myTemp = results.main.temp;
-        console.log(myTemp);
         $("#my-temp").html("Temperature: " + myTemp);
 
         // humidity
-        console.log(results.main.humidity)
         var myHumidity = results.main.humidity
         $("#my-humidity").html("Humidity: " + myHumidity);
 
         // wind speeed
-        console.log(results.wind.speed);
         var myWind = results.wind.speed;
         $("#my-wind").html("Wind Speed: " + myWind);
 
-        console.log(results.weather[0].icon);
+        // icons
         var iconcode = results.weather[0].icon
         var icon = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        console.log(icon);
         $(".weather-icon").attr("src", icon);
 
         // latitude and longiture for uv
         var lat = results.coord.lat;
         var lon = results.coord.lon;
-        console.log(lat, lon);
 
         // uv
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey,
             method: "GET"
         }).then(function (uvResults) {
-            console.log(uvResults);
-            console.log(uvResults.value);
             var myUv = uvResults.value;
-            console.log(myUv);
             $("#my-uv").html("UV Index: " + myUv);
 
             // 0-2 low, 3-7 moderate ,8+ very high  epa.gov
@@ -117,17 +104,6 @@ function loadWeather(myCity) {
             url: "https://api.openweathermap.org/data/2.5/forecast?q=" + myCity + "&&units=imperial&appid=" + apiKey,
             method: "GET"
         }).then(function (fiveDayResult) {
-            console.log(fiveDayResult);
-            console.log(fiveDayResult.list[0].main.temp);
-            var dayOneTemp = fiveDayResult.list[0].main.temp;
-            console.log(dayOneTemp);
-            var dayOneHumidity = fiveDayResult.list[0].main.humidity;
-            console.log(dayOneHumidity);
-
-            // var fiveDayIcon = fiveDayResult.list[0].weather[0].icon;
-            // var fiveIcon = "http://openweathermap.org/img/w/" + fiveDayIcon + ".png";
-            // console.log(fiveIcon);
-
 
             // send to html
             // day +1
@@ -144,7 +120,7 @@ function loadWeather(myCity) {
             var iconTwo = fiveDayResult.list[16].weather[0].icon;
             var iconurl2 = "http://openweathermap.org/img/w/" + iconTwo + ".png";
             $("#day2-icon").attr("src", iconurl2);
- 
+
 
             // day + 3
             $("#day3-temp").html("Temperature: " + fiveDayResult.list[24].main.temp);
